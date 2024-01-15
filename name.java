70443007be75890e9ddb95ff6906eba4955f3037
sarathchandrasimma <sarathchandrarocking@gmail.com>
2024-01-15 21:05:53 +0530

    new java file

---
+++ name.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class name{
+    public staic main void(String[] args)
+    {
+        int i;
+        for (i=0; i<11;i++){
+            Sytsem.out.pritnln("sarath chandra")
+        }
+    }
+}
